3702b3f50611d1bf7799dec13c0bb2677e2cef00
faresherif <149677526+faresherif@users.noreply.github.com>
2024-03-22 21:38:51 +0200

    Electro_Mech_7th_Proj._Test.ino

--- Electro_Mech_7th_Proj._Test.ino
+++ Electro_Mech_7th_Proj._Test.ino
@@ -9,14 +9,14 @@
 #include <LiquidCrystal_I2C.h>
 
 #define ENCODER_A_1 2                 //SHOULDER ENCODER PIN A (BASE JOINT) "INTERRUPT"
-#define ENCODER_B_1 3               //SHOULDER ENCODER PIN B (BASE JOINT)
-#define MOTOR_1_FORWARD  11      //SHOULDER MOTOR (BASE JOINT)
-#define MOTOR_1_BACKWARD 10     //SHOULDER MOTOR (BASE JOINT)
-#define POT_1 A3          //SHOULDER POSITION CONTROL (BASE JOINT)
-#define LCD_SDA A4     //LCD I2C DATA PIN
-#define LCD_SCL A5    //LCD I2C CLK PIN
+#define ENCODER_B_1 22               //SHOULDER ENCODER PIN B (BASE JOINT)
+#define MOTOR_1_FORWARD  5      //SHOULDER MOTOR (BASE JOINT)
+#define MOTOR_1_BACKWARD 4     //SHOULDER MOTOR (BASE JOINT)
+#define POT_1 A0          //SHOULDER POSITION CONTROL (BASE JOINT)
+#define MOTOR_SPEED 9   // MOTOR_1 ENABLE PIN 9
 
-int MIN_ANGLE=0,MAX_ANGLE=360, OLD_ANGLE_1,POS_1;
+volatile long POS_1;
+int MIN_ANGLE=0,MAX_ANGLE=360, OLD_ANGLE_1;
 LiquidCrystal_I2C LCD(0x27,16,2);
 
 void MOVE_MOTOR_ANGLE_1(int ANGLE) {
@@ -43,35 +43,35 @@ void READ_ENCODER_1(){
   else{
     POS_1--;
   }
+
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(ENCODER_A_1,INPUT);
-  pinMode(ENCODER_B_1,INPUT);
-  pinMode(MOTOR_1_FORWARD, OUTPUT);
-  pinMode(MOTOR_1_BACKWARD, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A_1),READ_ENCODER_1,CHANGE);
-  LCD.init(); //LCD I2C DATA PIN 20 , CLK PIN 21
-  LCD.backlight();
-  LCD.clear();
+Serial.begin(9600);
+pinMode(ENCODER_A_1,INPUT);
+pinMode(ENCODER_B_1,INPUT);
+pinMode(MOTOR_1_FORWARD, OUTPUT);
+pinMode(MOTOR_1_BACKWARD, OUTPUT);
+pinMode(MOTOR_SPEED, OUTPUT);
+attachInterrupt(digitalPinToInterrupt(ENCODER_A_1),READ_ENCODER_1,CHANGE);
+LCD.init(); //LCD I2C DATA PIN 20 , CLK PIN 21
+LCD.backlight();
+LCD.clear();
 }
 
 void loop() {
-  int POT_1_VALUE=analogRead(POT_1);
-  int ANGLE_MOTOR_1=map(POT_1_VALUE,0,1023,MIN_ANGLE,MAX_ANGLE);
-
-  MOVE_MOTOR_ANGLE_1(ANGLE_MOTOR_1);
-
-  
-  LCD.setCursor(0, 0);
+  analogWrite(MOTOR_SPEED,  100);
+int POT_1_VALUE=analogRead(POT_1);
+int ANGLE_MOTOR_1=map(POT_1_VALUE,0,1023,MIN_ANGLE,MAX_ANGLE);
+constrain(ANGLE_MOTOR_1, 0, 360);
+  LCD.setCursor(0,1);
   LCD.print("1st ");
   LCD.print(ANGLE_MOTOR_1);
-  LCD.print((char)223); // Degree symbol
-  LCD.setCursor(0, 1);
-  LCD.print("pot");
-  LCD.print(POT_1_VALUE);
-  delay(100); // Adjust delay as needed for display refresh rate
+  LCD.print((char)223); // DEGREE SYMBOL
+  delay(100);  
+MOVE_MOTOR_ANGLE_1(ANGLE_MOTOR_1);
+Serial.println(ANGLE_MOTOR_1);
 
 }
+
 
